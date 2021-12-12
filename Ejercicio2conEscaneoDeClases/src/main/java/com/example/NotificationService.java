87a560ed805c52268c1838079f7808ba2e6c9165
Juan José Hidalgo Neira <xxhidalgoneira@gmail.com>
2021-12-12 23:41:47 +0100

    Ejercicio 2 con Escaneo de Clases

---
+++ Ejercicio2conEscaneoDeClases/src/main/java/com/example/NotificationService.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+void saludo(){
+    System.out.println("Un cordial saludo");
+}
+
+}
